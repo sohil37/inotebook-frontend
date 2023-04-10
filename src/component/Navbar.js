d84e18a2229a0e978dadc6ae37bc314f748c387e
sohil37 <53380122+sohil37@users.noreply.github.com>
2023-04-10 11:32:34 +0530

    implemented useLocation hook

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,7 +1,12 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 function Navbar() {
+  let location = useLocation();
+  useEffect(() => {
+    console.log(location);
+  }, [location]);
+
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
@@ -21,12 +26,21 @@ function Navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
+              <Link
+                className={`nav-link ${
+                  location.pathname === "/" ? "active" : ""
+                }`}
+                aria-current="page"
+                to="/">
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/About">
+              <Link
+                className={`nav-link ${
+                  location.pathname === "/about" ? "active" : ""
+                }`}
+                to="/About">
                 About
               </Link>
             </li>
