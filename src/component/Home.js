ab0898170bb836fdad88ae937db170b6e688f733
sohil37 <53380122+sohil37@users.noreply.github.com>
2023-04-10 12:02:34 +0530

    added add notes and your notes ui

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,7 +3,25 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <h1>This is home</h1>
+      <h1 className="my-3">Add Note</h1>
+      <form>
+        <div class="mb-3">
+          <label for="title-field" class="form-label">
+            Title
+          </label>
+          <input type="text" class="form-control" id="title-field" />
+        </div>
+        <div class="mb-3">
+          <label for="description-field" class="form-label">
+            Description
+          </label>
+          <textarea class="form-control"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">
+          Add note
+        </button>
+      </form>
+      <h1 className="my-3">Your Notes</h1>
     </div>
   );
 }
